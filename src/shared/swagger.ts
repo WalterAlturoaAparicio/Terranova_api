9bc45d07623eafc004a340efbe449ffe1f68f406
walter <walteralturoaparicio@gmail.com>
2022-09-20 09:39:33 -0500

    daily 5 swagger corrections

--- src/shared/swagger.ts
+++ src/shared/swagger.ts
@@ -1,6 +1,9 @@
 import swaggerJSDoc from "swagger-jsdoc";
 import { version } from "../../package.json";
+import dotenv from "dotenv";
 
+dotenv.config();
+const port = process.env.PORT || 3000;
 const options = {
   swaggerDefinition: {
     openapi: "3.0.0",
@@ -9,12 +12,12 @@ const options = {
       version,
       description: "Documentacion Prueba Tecnica",
     },
-    servers: [{ url: "http://localhost:3000" }],
+    servers: [
+      { url: `http://localhost:${port}` },
+      { url: `https://vast-depths-48635.herokuapp.com` },
+    ],
   },
-  apis: [
-    "./src/shared/*.swagger.ts",
-    "./src/components/**/*.router.ts",
-  ],
+  apis: ["./src/shared/*.swagger.ts", "./src/components/**/*.router.ts"],
 };
 
 export const specs = swaggerJSDoc(options);
